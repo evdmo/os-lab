5145434b6e0b02735fc9204e33ac6863fcb3152f
evdmo <evd.mo.etu@gmail.com>
2025-03-02 14:33:26 +0300

    Delete 3ExamplesProc&Thread directory

--- 3ExamplesProc&Thread/ex_disp_signal/son_IGN.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <signal.h>
-
-int main(){
-    signal(SIGUSR1, SIG_IGN);   // Действие при получение сигнала SIGUSR1: игнорировать данный сигнал
-    sleep(5);   // Задержка, чтобы процесс не закончился быстрее отца
-    return 0;
-}
